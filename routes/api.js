e55b6c6b145af516913835a7f49cd48fb0adfffc
Raven Cole <ravenscole@gmail.com>
2017-01-24 13:26:23 -0500

    es6 refactor

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
-export default () => [
-    {
-        path: '/api',
-        verb: 'get',
-        callback: (_req, _res) => {
-            _res.send('666')
-        }
+const API = {
+    path: '/api',
+    verb: 'get',
+    callback(_req, _res) {
+        _res.send('666')
     }
-]
+}
+
+export default () => [ API ]
